ece9cd76328cd39f0f607377faf6fed83ba680f0
Albertmabo <albertmabo001@gmail.com>
2025-12-19 09:21:28 +0545

    feat: add role field to User schema (admin, vendor, user)

--- src/models/user.js
+++ src/models/user.js
@@ -4,28 +4,37 @@ const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
     required: true,
-    min: [3, "First Name cannot be be less then 3"],
-    max: [20, "First Name cannot be greater then 20 character"],
+    minlength: [3, "First Name cannot be be less then 3"],
+    maxlength: [20, "First Name cannot be greater then 20 character"],
   },
   middleName: {
     type: String,
-    min: [3, "Middle Name cannot be be less then 3"],
-    max: [20, "Middle Name cannot be greater then 20 character"],
+    minlength: [3, "Middle Name cannot be be less then 3"],
+    maxlength: [20, "Middle Name cannot be greater then 20 character"],
   },
   lastName: {
     type: String,
     required: true,
-    min: [3, "Middle Name cannot be be less then 3"],
-    max: [20, "Middle Name cannot be greater then 20 character"],
+    minlength: [3, "Middle Name cannot be be less then 3"],
+    maxlength: [20, "Middle Name cannot be greater then 20 character"],
   },
   email: {
     type: String,
     required: [true, "E-mail cannot be empty"],
+    unique: true,
   },
   phoneNo: {
     type: String,
     required: [true, "Phone No cannot be empty"],
   },
+
+  role: {
+    type: String,
+    enum: ["admin", "user", "vendor"],
+    default: "user",
+    required: true,
+  },
+
   address: {
     type: String,
     required: [true, "Address cannot be empty"],
@@ -36,6 +45,5 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-
-const User = mongoose.model("User",userSchema );
-export default User;``
+const User = mongoose.model("User", userSchema);
+export default User;
