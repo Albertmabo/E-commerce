fe55b199fcfe8a349c14064c96d48eb8a9753e06
Albertmabo <albertmabo001@gmail.com>
2025-12-19 12:04:15 +0545

    feat(models): add VendorShop schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,49 +1,54 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minlength: [3, "First Name cannot be be less then 3"],
-    maxlength: [20, "First Name cannot be greater then 20 character"],
-  },
-  middleName: {
-    type: String,
-    minlength: [3, "Middle Name cannot be be less then 3"],
-    maxlength: [20, "Middle Name cannot be greater then 20 character"],
-  },
-  lastName: {
-    type: String,
-    required: true,
-    minlength: [3, "Middle Name cannot be be less then 3"],
-    maxlength: [20, "Middle Name cannot be greater then 20 character"],
-  },
-  email: {
-    type: String,
-    required: [true, "E-mail cannot be empty"],
-    unique: true,
-  },
-  phoneNo: {
-    type: String,
-    required: [true, "Phone No cannot be empty"],
-  },
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: [3, "First Name cannot be be less then 3"],
+      maxlength: [20, "First Name cannot be greater then 20 character"],
+    },
+    middleName: {
+      type: String,
+      minlength: [3, "Middle Name cannot be be less then 3"],
+      maxlength: [20, "Middle Name cannot be greater then 20 character"],
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minlength: [3, "Last Name cannot be be less then 3"],
+      maxlength: [20, "Last Name cannot be greater then 20 character"],
+    },
+    email: {
+      type: String,
+      required: [true, "E-mail cannot be empty"],
+      unique: true,
+    },
+    phoneNo: {
+      type: String,
+      required: [true, "Phone No cannot be empty"],
+    },
 
-  role: {
-    type: String,
-    enum: ["admin", "user", "vendor"],
-    default: "user",
-    required: true,
-  },
+    role: {
+      type: String,
+      enum: ["admin", "user", "vendor"],
+      default: "user",
+      required: true,
+    },
 
-  address: {
-    type: String,
-    required: [true, "Address cannot be empty"],
+    address: {
+      type: String,
+      required: [true, "Address cannot be empty"],
+    },
+    dateOfBirth: {
+      type: Date,
+      required: [true, "Date of birth cannot be empty"],
+    },
   },
-  dateOfBirth: {
-    type: Date,
-    required: [true, "Date of birth cannot be empty"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 export default User;
---
+++ src/models/vendorShop.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const vendorShopSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    shopName: {
+      type: String,
+      mixlength: [30, "Shop name cannot be more then 30"],
+      minlength: [3, "Shop name cannot be less then 2"],
+      required: [true, "Shop Name cannot be empty"],
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    ratings: {
+      type: String,
+    },
+    shopRegistrationNumber: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const VendorShop = mongoose.model("VendorShop", vendorShopSchema);
+export default VendorShop;
