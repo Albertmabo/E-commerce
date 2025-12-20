7b717be15f5441c87534a24534cbfb85430cc7d5
Albertmabo <albertmabo001@gmail.com>
2025-12-20 09:49:57 +0545

    feat(controllers): add vendorShop controller
    Implemented CRUD operations for vendorShop

---
+++ src/controllers/vendor.controller.js
@@ -0,0 +1,135 @@
+import VendorShop from "../models/vendorShop.js";
+import User from "../models/user.js";
+
+//@desc Get VendorShop
+//@rotute GET api/v1/vendorshop
+//@access public
+
+const getAllVendorShops = async (req, res) => {
+  const vendorShops = await VendorShop.find({}).select("-__v").populate({
+    path: "user",
+    select: "-__v",
+  });
+  res.status(200).json({
+    success: true,
+    message: "Vendor Shop retrived successfully",
+    data: vendorShops,
+    meta: {
+      total: vendorShops.length,
+    },
+  });
+};
+
+//@desc Get sinble VendorShop
+//@rotute GET api/v1/vendorshop/:id
+//@access public
+
+const getVendorShop = async (req, res) => {
+  const { id: vendorId } = req.params;
+  const vendorShop = await VendorShop.findById(vendorId)
+    .select("-__v")
+    .populate({
+      path: "user",
+      select: "-__v",
+    });
+
+  if (!vendorShop) {
+    return res.status(404).json({
+      error: "VendorShop not found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Vendor Shop retrived successfully",
+    data: vendorShop,
+  });
+};
+
+//@desc Post VendorShop
+//@rotute POST api/v1/vendorshop
+//@access public
+
+const createVendorShop = async (req, res) => {
+  const {
+    user: userId,
+    shopName,
+    address,
+    ratings,
+    shopRegistrationNumber,
+    isVerified,
+  } = req.body;
+  // Check if the user exist
+  const user = await User.findOne({ _id: userId });
+
+  console.log(user);
+
+  if (!user) {
+    return res.status(404).json({
+      error: "user not found",
+    });
+  }
+
+  if (user.role !== "vendor") {
+    return res.status(400).json({
+      error: "User is not a vendor",
+    });
+  }
+
+  const vendorShop = await VendorShop.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    message: "Vendor Shop created successfully",
+    data: vendorShop,
+  });
+};
+//@desc Update VendorShop
+//@rotute PATCH api/v1/vendorshop/:id
+//@access public
+
+const updatedVendorShop = async (req, res) => {
+  const { id: vendorId } = req.params;
+  const vendorShop = await VendorShop.findOneAndUpdate(
+    { _id: vendorId },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!vendorShop) {
+    return res.status(404).json({
+      error: "No vendot shop avalilabe",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Vendor Shop Updated successfully",
+    data: vendorShop,
+  });
+};
+//@desc Delete VendorShop
+//@rotute DELETE api/v1/vendorshop/:id
+//@access public
+
+const deleteVendorShop = async (req, res) => {
+  const { id: vendorId } = req.params;
+
+  const vendorShop = await VendorShop.findOneAndDelete(vendorId);
+
+  res.status(200).json({
+    success: true,
+    message: "Vendor Shop Deleted successfully",
+    data: null
+  });
+};
+export {
+  getAllVendorShops,
+  getVendorShop,
+  createVendorShop,
+  updatedVendorShop,
+  deleteVendorShop
+};
---
+++ src/routes/vendor.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router()
+
+import {getAllVendorShops, getVendorShop,  updatedVendorShop, createVendorShop, deleteVendorShop } from "../controllers/vendor.controller.js"
+
+router.route('/').get(getAllVendorShops).post(createVendorShop);
+router.route('/:id').get(getVendorShop).patch(updatedVendorShop).delete(deleteVendorShop);
+
+export default router;
