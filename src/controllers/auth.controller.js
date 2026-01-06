10d4eb8f5fac7dc18314e682b46825eb516dcf5c
Albertmabo <albertmabo001@gmail.com>
2026-01-06 08:53:28 +0545

    feat(auth): implement login controller returning JWT token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -22,5 +22,33 @@ const signUpUser = asyncErrorHandler(async (req, res) => {
   });
 });
 
+//@desc login User
+//@route POST api/v1/users/login
+//@access Public
 
-export { signUpUser};
+const logInUser = asyncErrorHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new CustomError("Please provide email and password", 400);
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    throw new CustomError("Invalid email or password", 401);
+  }
+
+  const isMatch = await user.comparePasswordInDb(password, user.password);
+  if (!isMatch) {
+    throw new CustomError("Invalid email or password", 401);
+  }
+
+  const token = signToken(user._id, user.role);
+
+  res.status(200).json({
+    success: true,
+    message: "User logged in successfully",
+    data: user,
+    token,
+  });
+});
+
+export { signUpUser, logInUser };
