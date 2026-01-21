2e617f5ab3fd796ac061210bfdb0605bd0167b53
Albertmabo <albertmabo001@gmail.com>
2026-01-21 13:56:02 +0545

    security(app): limit JSON request body to 30kb

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 import express from "express";
-import helmet  from "helmet";
+import helmet from "helmet";
 const app = express();
 
 // routes import
-import rateLimiter from  "./src/middlewares/rateLimiting.middleware.js"
+import rateLimiter from "./src/middlewares/rateLimiting.middleware.js";
 import productRoutes from "./src/routes/product.routes.js";
 import authRoutes from "./src/routes/auth.routes.js";
 import vendorRoutes from "./src/routes/vendorShop.routes.js";
-import CustomError from "./src/utils/CustomError.js"
-import routeNotFound from "./src/middlewares/routeNotFound.middleware.js"
-import globalErrorHandler from "./src/middlewares/globalErrorHandler.middleware.js"
+import CustomError from "./src/utils/CustomError.js";
+import routeNotFound from "./src/middlewares/routeNotFound.middleware.js";
+import globalErrorHandler from "./src/middlewares/globalErrorHandler.middleware.js";
 
 // middleware
 app.use(helmet());
-app.use(express.json());
-app.use("/api", rateLimiter(1000, 60 * 60 * 1000))
+app.use(express.json({ limit: "30kb" }));
+app.use("/api", rateLimiter(1000, 60 * 60 * 1000));
 // routes
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/users", authRoutes);
 app.use("/api/v1/vendors", vendorRoutes);
 
+app.use("/test", (req, res, next) => {
+  throw new CustomError(`Testing cusotmerror and global error`, 400);
+});
 
-app.use("/test", (req,res,next)=>{
-    throw new CustomError(`Testing cusotmerror and global error`, 400)
-})
-
-// rotue not found 
-app.use(routeNotFound)
-// Global error handler 
-app.use(globalErrorHandler)
+// rotue not found
+app.use(routeNotFound);
+// Global error handler
+app.use(globalErrorHandler);
 export default app;
