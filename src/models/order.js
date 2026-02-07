d89f56b4b328a9bac540daa3557769a8884a311b
Albertmabo <albertmabo001@gmail.com>
2026-02-07 16:15:25 +0545

    feat(models): add order schema

---
+++ src/models/order.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "User must be asscociated with a Orders"]
+    },
+    items: [
+        {
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+                required: [true, "Please provide the product for the order"]
+            },
+            quantity: {
+                type: Number,
+                required: [true, "Please provide the product quantity"],
+                validate: {
+                    validator: function (value) {
+                        return value > 0;
+                    },
+                    message: "Product quantity cannot be Zero or less then 0"
+                }
+            }
+        }
+    ],
+    total: {
+        type: Number,
+        required: [true, "Total price cannot be empty"]
+    },
+    payment: {
+        paymentType: {
+            type: String,
+            enum: ["COD", "MobileBanking"],
+            required: [true, "Must provide Payment type"]
+        },
+        prePaid: {
+            type: Boolean,
+            default: false,
+
+        }
+    }
+})
+
+orderSchema.pre('save', function(){
+    if(this.payment.paymentType === "MobileBanking"){
+        this.payment.prePaid = true;
+    }
+})
+
+const Order = mongoose.model("Order", orderSchema);
+export default Order;
