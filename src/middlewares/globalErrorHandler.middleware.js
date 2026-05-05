f370e990718be614e6cd93337476ba4a215909f9
Albertmabo <albertmabo001@gmail.com>
2026-05-05 16:21:11 +0545

    feat(globalErrorHandler): add environment-specific responses for development and production

--- src/middlewares/globalErrorHandler.middleware.js
+++ src/middlewares/globalErrorHandler.middleware.js
@@ -1,12 +1,39 @@
-const globalErrorHandler = (error, req, res, next)=>{
-    error.statusCode = error.statusCode || 500
-    error.status = error.status || "error"
-    
-    res.status(error.statusCode).json({
-        status: error.status,
-        message: error.message
+const globalErrorHandler = (error, req, res, next) => {
+  error.statusCode = error.statusCode || 500;
+  error.status = error.status || "error";
+  // for dev
 
-    })
-}
+  const devErrors = (res, error) => {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+      stackTrace: error.stack,
+      error: error,
+    });
+  };
+
+  // for prod
+
+  const prodError = (res, error) => {
+    // send this if the error is operational error
+    if (error.isOperational) {
+      res.status(error.statusCode).json({
+        status: error.status,
+        message: error.message,
+      });
+    }else{
+      res.status(500).json({
+        status: "error",
+        message:"Somthing whent wrong please try again later"
+      })
+    }
+
+  };
+  if (process.env.NODE_ENV === "development") {
+    devErrors(res, error);
+  } else if (process.env.NODE_ENV === "production") {
+    prodError(res, error);
+  }
+};
 
 export default globalErrorHandler;
